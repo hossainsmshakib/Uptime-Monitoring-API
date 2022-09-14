aec8d51afcf84ac2f312bd46c3b217a548f789f4
sm-shakib <76900961+sm-shakib@users.noreply.github.com>
2022-09-14 22:15:15 +0600

    created a server

--- index.js
+++ index.js
@@ -4,14 +4,24 @@ const http = require('http');
 // app obj
 const app = {};
 
+// config
+app.config = {
+    port: 7000,
+};
+
 // create server
 app.createServer = () => {
-    const server = http.createServer(handleReqRes);
+    const server = http.createServer(app.handleReqRes);
+    server.listen(app.config.port, () => {
+        console.log(`Listening to port ${app.config.port}`);
+    });
 };
 
 //handle req response
 app.handleReqRes = (req, res) => {
     // response handle
-    res.end('Hello World!');
+    res.end('Hello Node');
 };
 
+//start server
+app.createServer();
