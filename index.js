f56babecf5985c47545ddc80000190357bf89e3e
sm-shakib <76900961+sm-shakib@users.noreply.github.com>
2022-09-14 22:03:08 +0600

    changes

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-console.log('Hello 005');
-console.log('Hello!');
-console.log('Hello 005');
-console.log('Hello!');
+// dependecies
+const http = require('http');
+
+// app obj
+const app = {};
+
+// create server
+app.createServer = () => {
+    const server = http.createServer(handleReqRes);
+};
+
+//handle req response
+app.handleReqRes = (req, res) => {
+    // response handle
+    res.end('Hello World!');
+};
+
