ed05c779a051deab436333e6488903a84263e991
sm-shakib <76900961+sm-shakib@users.noreply.github.com>
2022-09-16 12:08:03 +0600

    create data in json

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 // dependecies
 const http = require('http');
 const {handleReqRes} =require('./helpers/handleResReq');
-const environment = require('./helpers/environments')
+const environment = require('./helpers/environments');
+const data = require('./lib/data');
 // app obj
 const app = {};
+
+// testing file system
+data.create('test', 'newFile', {name: 'Bangladesh', language: 'Bangla'}, (err) => {
+    console.log(`error was`, err);
+});
 
 // create server
 app.createServer = () => {
